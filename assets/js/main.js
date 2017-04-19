6a35567acd941a32554061c4499afca6bc3c3007
ely211 <gonzalesm405@gmail.com>
2017-04-19 13:19:54 -0500

    desafío V

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var container = document.getElementsByClassName("container")[0];
+var pink = document.getElementById("pink");
+var blue = document.getElementById("blue");
+var green =  document.getElementById("green");
+var yellow = document.getElementById("yellow");
+
+container.style.position = "relative";
+container.style.background = "DarkGrey";
+container.style.width = "37.5rem";
+container.style.padding = "3.13rem";
+
+blue.style.position = "absolute";
+blue.style.top = "15.63rem";
+blue.style.right = "0";
